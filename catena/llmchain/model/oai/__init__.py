880040ef7d50549cbc9cc7bece7fe40747a0fb21
Asianfleet <1239326528@qq.com>
2025-01-24 17:01:24 +0800

    refactor(llmchain): 重构模型模块以支持更多 AI 提供商
    - 删除了 catena/llmchain/model/models.py 文件，清理了大量冗余代码
    - 新增 catena/llmchain/model/oai/compatible.py 文件，实现对 OpenAI 兼容模型的支持
    - 更新了 catena/llmchain/model/oai/__init__.py 文件，引入新的 OpenAI_C 类

--- catena/llmchain/model/oai/__init__.py
+++ catena/llmchain/model/oai/__init__.py
@@ -1,1 +1,7 @@
 from .oai import OpenAIOrigin
+from .compatible import OpenAI_C
+
+__all__ = [
+    "OpenAIOrigin",
+    "OpenAI_C",
+]
---
+++ catena/llmchain/model/oai/compatible.py
@@ -0,0 +1,49 @@
+import os
+from enum import Enum
+from packaging import version
+from pydantic import BaseModel, ConfigDict
+from pydantic import Field, model_validator
+from typing import (
+    Any, 
+    Dict, 
+    Iterator, 
+    List, 
+    Optional, 
+    Union
+)
+
+from .oai import OpenAIOrigin
+from ..base import Model
+from ..metrics import Metrics
+from ...response import ModelResponse
+from ...message import Message, MessageBus
+from ....error.modelerr import ModelError
+from ....catenasmith.cli_tools import info, warning
+from ....catena_core.alias.builtin import (
+    NodeType as Ntype, 
+    ModelProvider as Provider
+)
+from ....catena_core.callback import NodeCallback
+from ....catena_core.node.completion import NodeCompletion
+
+class OpenAI_C(OpenAIOrigin):
+    
+    # 模型名称
+    model: str = "gpt-4o-mini"
+    # 模型提供商
+    provider: Enum = Field(default=Provider.OpenAIC, init=False)
+    # 模型 URL
+    base_url: Optional[str] = None
+    # 模型 API Key
+    api_key: Optional[str] = None
+    
+    # 内部参数。不用于 API 请求
+    # 是否使用结构化输出与此模型。
+    structured_outputs: bool = False
+    # 模型是否支持结构化输出。
+    supports_structured_outputs: bool = True
+    
+    model_config = ConfigDict(
+        protected_namespaces=(),
+        arbitrary_types_allowed=True
+    )
